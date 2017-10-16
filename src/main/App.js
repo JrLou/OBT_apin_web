d777e8df0e9d93968b9fca6ee800439f93c297bc
myetc <owqdmjep@163.com>
2017-10-16 18:10:49 +0800

    1:修改样式

--- src/main/App.js
+++ src/main/App.js
@@ -12,9 +12,7 @@ class page extends Component{
     render(){
         return(
             <Layout>
-                <Header style={{backgroundColor:"#fff"}}>
-                    <AppTop root={this}/>
-                </Header>
+                <AppTop root={this}/>
                 <Content style={{marginTop:10}}>
                     {this.props.children}
                 </Content>
