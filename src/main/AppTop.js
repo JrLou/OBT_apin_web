2bf5651817cef56c4dda2621a503fd8206c07513
荒原N旅人 <leo_nyh@sina.com>
2017-11-04 09:27:18 +0800

    页面

--- src/main/AppTop.js
+++ src/main/AppTop.js
@@ -1,65 +1,72 @@
 /**
  * Created by lixifeng on 17/3/24.
  */
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Button } from 'antd';
 
-import {Button} from 'antd';
+import Sign from './body/component/SignView';
+
 import less from "./AppTop.less";
- class page extends Component {
+
+class page extends Component {
 
     constructor(props) {
         super(props);
     }
 
-    showModal(visible) {
-    }
-
     render() {
         return (
             <div className={less.head}>
-               <div className={less.headContent}>
-                   <a>
-                       <img
-                           onClick={() => {
-                               log(this.props.root);
-                               window.app_open(this.props.root, "/",null,"self");
-                           }}
-                           className={less.left}
-                           src={require('../images/index_logo.png')}
-                       />
-                   </a>
-                   <div className={less.right}>
-                       <div className={less.rightItem}>
-                           {/*<img*/}
-                               {/*className={less.rightIcon}src={require('../images/phone.png')}*/}
-                           {/*/>*/}
-                           <Button type="primary" className={less.login}
-                            onClick={()=>{
-                                window.open("http://mvp.apin.com");
+                <div className={less.headContent}>
+                    <a>
+                        <img
+                            onClick={() => {
+                                log(this.props.root);
+                                window.app_open(this.props.root, "/", null, "self");
                             }}
-                           >登录</Button>
-                           <Button type="primary" className={less.register}
-                                   onClick={()=>{
-                                       window.open("http://mvp.apin.com/#/TestPage");
-                                   }}
-                           >注册</Button>
+                            className={less.left}
+                            src={require('../images/index_logo.png')}
+                        />
+                    </a>
+                    <div className={less.right}>
+                        <div className={less.rightItem}>
+                            {/*<img*/}
+                            {/*className={less.rightIcon}src={require('../images/phone.png')}*/}
+                            {/*/>*/}
+                            {/*<Button type="primary" className={less.login}
+                                onClick={() => {
+                                    window.open("http://mvp.apin.com");
+                                }}
+                            >登录</Button>*/}
+                            <Button type="primary" className={less.login}
+                                onClick={() => this.modal.showModal(0)}
+                            >登录2</Button>
+                            {/*<Button type="primary" className={less.register}
+                                onClick={() => {
+                                    window.open("http://mvp.apin.com/#/TestPage");
+                                }}
+                            >注册</Button>*/}
+                            <Button type="primary" className={less.register}
+                                onClick={() => this.modal.showModal(1)}
+                            >注册</Button>
 
-                       </div>
-                       <div className={less.rightItem}
+                        </div>
+                        <div className={less.rightItem}
                             title="开发测试：打开订单列表"
-                            onClick={()=>{
+                            onClick={() => {
                                 // window.open("http://mvp.apin.com");
                                 window.app_open(this, "/OrderFormList", {
-                                    data:{}
-                                },"new");
+                                    data: {}
+                                }, "new");
                             }}
-                       >
-                           <font style={{fontSize:14,color:"#666"}}>客服电话</font>
-                           <br/>
-                           <font style={{fontSize:16,color:"#34b0ff"}}>0571-58122998</font>
-                       </div>
-                   </div>
-               </div>
+                        >
+                            <font style={{ fontSize: 12, color: "#666" }}>客服电话  9:00~21:00</font>
+                            <br />
+                            <font style={{ fontSize: 16, color: "#34b0ff" }}>0571-58122998</font>
+                        </div>
+                    </div>
+                </div>
+                <Sign ref={(modal) => this.modal = modal}></Sign>
             </div>
         );
     }
