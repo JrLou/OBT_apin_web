78ef7c44c061660653c35bf89a0b2725546dd3eb
huazhilin <zhilingege@gmail.com>
2017-11-07 11:16:01 +0800

    还未完成接口的模拟和交互的优化

--- src/main/body/bank/BankUpload.js
+++ src/main/body/bank/BankUpload.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
-import {Input, Form, Icon, message, Modal, Upload} from 'antd';
+import {Input, Button, Icon, message, Modal, Upload} from 'antd';
+import Panel from '../../../pay/Panel';
 import less from "./BankUpload.less";
 
 
@@ -32,6 +33,10 @@ class BankUpload extends Component {
          this.setState({
             error: "缺少支付信息"
          });
+      } else {
+         this.setState({
+            transferAmount:data.price/100
+         });
       }
    }
 
@@ -47,7 +52,7 @@ class BankUpload extends Component {
    getdefaultView() {
       const iptProps = {
          size: "large",
-         className: less.form_control
+         className: less.form_control,
       };
       return (
          <div>
@@ -96,35 +101,37 @@ class BankUpload extends Component {
                            onChange={(e) => {
                               this.setFomrFileds(e, "cardNum");
                            }}
-                           prefix={<Icon type="credit-card" style={{fontSize: 13}}/>} placeholder={"请输入账户名"}
+                           prefix={<Icon type="credit-card" style={{fontSize: 13}}/>} placeholder={"请输入卡号"}
                         />
                      </div>
                   </div>
 
                   <div className={less.container2Son}>
                      <div className={less.form_group + " " + less.fl}>
-                        <label htmlFor="accountName" className={less.control_label}>银行：</label>
+                        <label htmlFor="bankName" className={less.control_label}>银行：</label>
                         <br/>
                         <Input
                            {...iptProps}
-                           id="accountName"
+                           id="bankName"
                            onChange={(e) => {
-                              this.setFomrFileds(e, "accountName");
+                              this.setFomrFileds(e, "bankName");
                            }}
-                           prefix={<Icon type="bank" style={{fontSize: 13}}/>} placeholder={"请输入账户名"}
+                           prefix={<Icon type="bank" style={{fontSize: 13}}/>} placeholder={"请输入银行名"}
                         />
                      </div>
 
                      <div className={less.form_group + " " + less.fl}>
-                        <label htmlFor="cardNum" className={less.control_label}>转账金额：</label>
+                        <label htmlFor="transferAmount" className={less.control_label}>转账金额：</label>
                         <br/>
                         <Input
                            {...iptProps}
-                           id="cardNum"
+                           id="transferAmount"
+                           defaultValue={this.state.transferAmount + "元"}
+                           disabled={true}
                            onChange={(e) => {
-                              this.setFomrFileds(e, "cardNum");
+                              this.setFomrFileds(e, "transferAmount");
                            }}
-                           prefix={<Icon type="pay-circle" style={{fontSize: 13}}/>} placeholder={"请输入账户名"}
+                           prefix={<Icon type="pay-circle" style={{fontSize: 13}}/>}
                         />
                      </div>
                   </div>
@@ -134,11 +141,43 @@ class BankUpload extends Component {
                <div className={less.bankBox_top}>上传转账证明</div>
                <div className={less.bankBox_middle}>
                   <div>
-                     {/*<UploadComponent/>*/}
-                     <PicturesWall/>
+                     <UploadCmp/>
                   </div>
                </div>
             </div>
+            <div className={less.txtCenter}>
+               <Button
+                  type={"primary"}
+                  className={less.submitBtn}
+                  onClick={()=>{
+                     //
+                     this.panel.show();
+                  }}
+               >
+                  提交
+               </Button>
+            </div>
+            <Panel
+               onAction={(action, showType) => {
+
+                  if (showType === "paying") {
+                     //验证是否支付
+                     this.handVer("pay", action);
+                  } else if (showType === "unioning") {
+                     //验证是否开通
+                     this.handVer("union");
+                  }
+                  else if (action === "ok" && showType === "success") {
+                     //打开订单页
+                     window.app_open(this, "/Order", {
+                        id: this.id
+                     }, "self");
+                  }
+
+               }}
+               ref={(ref) => {
+                  this.panel = ref;
+               }}/>
          </div>
       );
    }
@@ -149,6 +188,17 @@ class BankUpload extends Component {
             {this.state.error}
          </div>
       );
+   }
+
+   //模拟上传的接口
+   loadPayOrder(param, cb) {
+      setTimeout(() => {
+         let code = (Math.random() * 10).toFixed(0) - 1;
+         let data = {};
+         data.url = "http://www.baidu.com";
+         data.payPrice = (Math.random() * 10000).toFixed(0);
+         cb(code, "无库存了/或者其他", data);
+      }, Math.random() * 1000 + 2000);
    }
 
    render() {
@@ -163,179 +213,81 @@ class BankUpload extends Component {
    }
 }
 
-/**** ↓这个版本用的时正常可以兼容的↓ *******/
-class UploadComponent extends React.Component {
+class UploadCmp extends Component {
    constructor(props) {
       super(props);
       this.state = {
          previewVisible: false,
          previewImage: '',
-         fileList: [
-            // {
-            //    uid: -1,
-            //    name: 'xxx.png',
-            //    status: 'done',
-            //    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-            // }
-         ],
-         pzUrl: "",
+         fileList: [],
       };
-      this.isIMG = true;
+   }
+
+   isImageTypeOk(file) {
+      if (!/image\/\w+/.test(file.type)) {
+         message.warning("凭证必须为图片格式");
+         return false;
+      }
+      if (file.size >= 1024000 * 2) {
+         message.warning("图片过大，最大允许2M。");
+         return false;
+      }
+      return true;
    }
 
    beforeUpload(file) {
-      if (file) {
-         if (!/image\/\w+/.test(file.type)) {
-            message.warning("凭证必须为图片格式");
-            return false;
-         }
-         if (file.size >= 1024000 * 2) {
-            message.warning("图片过大，最大允许2M。");
-            return false;
-         }
-      }
-      let r = new FileReader();
-      r.readAsDataURL(file);
-      r.onload = (e) => {
-         let image_base64 = e.target.result;
+      return this.isImageTypeOk(file) ? true : false;
 
-         this.setState({
-            fileList: [...this.state.fileList, {
-               uid: new Date().getTime(),
-               name: file.name,
-               status: 'done',
-               thumbUrl: image_base64,
-            }],
-            pzUrl: image_base64
-         }, () => {
-         });
-         return false;
-      };
-      return false;
-   }
 
-   handleChange({fileList}) {
-      if (!this.isIMG) {
-         return;
-      }
-      console.log(fileList);
-      this.setState({fileList}, () => {
-         console.log(typeof this.state.fileList);
-      });
-      if (fileList[0] && (fileList[0].status === "done" || fileList[0].status === "error")) {
-         let pzUrl = fileList[0] ? fileList[0].thumbUrl : "";
-         this.setState({pzUrl});
-      }
-   }
+      // const isJPG = file.type === 'image/jpeg';
+      // if (!isJPG) {
+      //    message.error('凭证必须为图片格式');
+      // }
+      // const isLt2M = file.size / 1024 / 1024 < 2;
+      // if (!isLt2M) {
+      //    message.error('图片过大，最大允许2M。');
+      // }
+      // return isJPG && isLt2M;
 
-   handleRemove(file) {
-      this.setState({pzUrl: ""});
-   }
-
-   render() {
-      const {previewVisible, previewImage, fileList} = this.state;
-      const uploadButton = (
-         <div>
-            <Icon type="plus" style={{fontSize: 26, color: '#08c'}}/>
-            <h1 className="ant-upload-text">上传转账凭证</h1>
-         </div>//
-      );
-      return (
-         <div className="clearfix forUploadStyle">
-            <Upload
-               action='//jsonplaceholder.typicode.com/posts/'
-               listType="picture-card"
-               fileList={fileList}
-               beforeUpload={this.beforeUpload.bind(this)}
-               onChange={this.handleChange.bind(this)}
-               onRemove={this.handleRemove.bind(this)}
-            >
-               {fileList.length >= 6 ? null : uploadButton}
-            </Upload>
-         </div>
-      );
-   }
-}
-
-class PicturesWall extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         previewVisible: false,
-         previewImage: '',
-         fileList: [
-            // {
-            //    uid: -1,
-            //    name: 'xxx.png',
-            //    status: 'done',
-            //    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-            // }
-         ],
-      };
-   }
-
-   beforeUpload(file) {
-      if (file) {
-         if (!/image\/\w+/.test(file.type)) {
-            message.warning("凭证必须为图片格式");
-            return false;
-         }
-         if (file.size >= 1024000 * 2) {
-            message.warning("图片过大，最大允许2M。");
-            return false;
-         }
-      }
-      let r = new FileReader();
-      r.readAsDataURL(file);
-      r.onload = (e) => {
-         let image_base64 = e.target.result;
-         let tempObj = JSON.parse(JSON.stringify(this.state.fileList));
-         console.log(tempObj);
-         tempObj[0] = {
-            uid: -1312,
-            name: 'xxx.png',
-            status: 'done',
-            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-         };
-         tempObj.push({
-            uid: new Date().getTime(),
-            name: file.name,
-            status: 'done',
-            thumbUrl: image_base64,
-         });
-         console.log(tempObj);
-         this.setState({
-            fileList: tempObj,
-            pzUrl: image_base64
-         }, () => {
-            console.log(this.state.fileList);
-         });
-         return false;
-      };
-      return false;
+      // let r = new FileReader();
+      // r.readAsDataURL(file);
+      // r.onload = (e) => {
+      //    let image_base64 = e.target.result;
+      //    this.setState({
+      //       fileList: [{
+      //          uid: new Date().getTime(),
+      //          name: file.name,
+      //          status: 'done',
+      //          thumbUrl: image_base64,
+      //       }],
+      //       pzUrl: image_base64
+      //    }, () => {
+      //       console.log(this.state.fileList);
+      //    });
+      //    return false;
+      // };
+      // return false;//自动上传
    }
 
    handleCancel() {
       this.setState({previewVisible: false});
    }
 
-   handlePreview(file) {
-      this.setState({
-         previewImage: file.url || file.thumbUrl,
-         previewVisible: true,
-      });
-   }
-
    handleChange({fileList}) {
-      this.setState({fileList});
+      this.setState({fileList}, () => {
+         console.log("arguments");
+         console.log(arguments);
+      });
+
    }
 
    render() {
       const {previewVisible, previewImage, fileList} = this.state;
       const uploadButton = (
          <div>
-            <Icon type="plus"/>
-            <div className="ant-upload-text">Upload</div>
+            <Icon className={less.upload_text1} type="plus"/>
+            <div className={less.upload_text2}>点击此处上传转账凭证</div>
+            <div className={less.upload_text3}>最多添加 6 张</div>
          </div>
       );
       return (
@@ -345,7 +297,6 @@ class PicturesWall extends Component {
                listType="picture-card"
                fileList={fileList}
                beforeUpload={this.beforeUpload.bind(this)}
-               onPreview={this.handlePreview.bind(this)}
                onChange={this.handleChange.bind(this)}
             >
                {fileList.length >= 6 ? null : uploadButton}
