eea8e38256ec6239e6afcf0d3571471dfbf61c20
jr_lou <sovvjsai@163.com>
2017-11-09 16:20:25 +0800

    1.完成接口：乘机人新增／删除／修改／列表／确认

--- src/api/APILXD.js
+++ src/api/APILXD.js
@@ -4,6 +4,13 @@
 let APILXD = {
     //新增乘客
     addPassenger:'/bo/orderapi/v1.0/orders/passengers/add',
-
+    //删除乘客
+    deletePassenger:'/bo/orderapi/v1.0/orders/passengers/delete',
+    //加载乘机人列表
+    loadPassengerList:'/bo/orderapi/v1.0/orders/passengers/list',
+    //修改乘客
+    upDataPassenger:'/bo/orderapi/v1.0/orders/passengers/update',
+    //确认乘机人信息
+    confirmPassenger:'/bosj/orderapi/v1.0/orders/confirmpassenger',
 };
 module.exports = APILXD;
