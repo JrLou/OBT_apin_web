25d99e4f433e19f134f9b7ab5d8ae0aef44630da
myetc <owqdmjep@163.com>
2017-11-18 17:53:54 +0800

    1:整合代码

--- public/js/debug.js
+++ public/js/debug.js
@@ -1,10 +1,26 @@
-
-function loadScript(url, apikey) {
-    var script = document.createElement("script");
-    script.type = "text/javascript";
+function loadFunDebugScript(url, apikey,callback) {
+    var d = document, script = d.createElement("script");
+    script.type = "application/javascript";
+    if (typeof (callback) != "undefined") {
+        if (script.readyState) {
+            script.onreadystatechange = function () {
+                if (script.readyState == "loaded" || script.readyState == "complete") {
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        } else {
+            script.onload = function () {
+                callback();
+            };
+        }
+    }
     script.src = url;
     script.setAttribute("apikey", apikey);
-    document.body.appendChild(script);
+    d.body.appendChild(script);
+}
+
+loadFunDebugScript("https://og6593g2z.qnssl.com/fundebug.0.3.3.min.js", "9118aceecbca5f72a7d6d801ab1196a34f59af72067ef4fa6161d05ce3006cc7",function () {
     fundebug.appversion = "1.8.0";
     //
     if(window.location.hostname.indexOf("www.apin.com")>=0){
@@ -12,6 +28,4 @@ function loadScript(url, apikey) {
     }else{
         fundebug.releasestage = "development";
     }
-}
-
-loadScript("https://og6593g2z.qnssl.com/fundebug.0.3.3.min.js", "9118aceecbca5f72a7d6d801ab1196a34f59af72067ef4fa6161d05ce3006cc7");
+});
