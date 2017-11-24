88107f38b9145146373ffcabb3ec21bef7b2aecf
sumweal <sumweal@qq.com>
2017-11-24 16:17:30 +0800

    修改公共文件

--- src/api/APIGYW.js
+++ src/api/APIGYW.js
@@ -5,73 +5,44 @@ let APIGYW = {
     /**
      * 城市对搜索
      */
-    flightapi_flight_query: "/ba/flightapi/v1.0/flight/query",
-
-// /os/hotcityapi/v1.0/arrCity/list  ->/ba/manageapi/v1.0/arrCity/list
-// /os/airlineapi/v1.0/bestList ->/ba/manageapi/v1.0/bestList
-// /os/hotcityapi/v1.0/depCity/list ->/ba/manageapi/v1.0/depCity/list
-// /os/airlineapi/v1.0/list ->/ba/manageapi/v1.0/list
-    manageapi_arrCity_list: "/ba/manageapi/v1.0/arrCity/list",
-    manageapi_bestList: "/ba/manageapi/v1.0/bestList",
-    manageapi_depCity_list: "/ba/manageapi/v1.0/depCity/list",
-    manageapi_list: "/ba/manageapi/v1.0/list",
-
+    flightapi_flight_query:     "/ba/flightapi/v1.0/flight/query",
+    manageapi_arrCity_list:     "/ba/manageapi/v1.0/arrCity/list",
+    manageapi_bestList:         "/ba/manageapi/v1.0/bestList",
+    manageapi_depCity_list:     "/ba/manageapi/v1.0/depCity/list",
+    manageapi_list:             "/ba/manageapi/v1.0/list",
     /**
      * 根据城市对与出发日期结束日期查询航班详情
      */
-    flightapi_flightDetail_query: "/ba/flightapi/v1.0/flightDetail/query",
-
-
-    //左日历月份
-    flightapi_flightDetail_month_query: "/ba/flightapi/v1.0/flightDetail/month/query",
-
-    //右日历月份
-    flightapi_retFlight_month_query: "/ba/flightapi/v1.0/retFlight/month/query",
-
-    //左日期
-    flightapi_flightDetail_day_query: "/ba/flightapi/v1.0/flightDetail/day/query",
-
+    flightapi_flightDetail_query:       "/ba/flightapi/v1.0/flightDetail/query",
+    flightapi_flightDetail_month_query: "/ba/flightapi/v1.0/flightDetail/month/query",  //左日历月份
+    flightapi_retFlight_month_query:    "/ba/flightapi/v1.0/retFlight/month/query",     //右日历月份
+    flightapi_flightDetail_day_query:   "/ba/flightapi/v1.0/flightDetail/day/query",    //左日期
     /**
      * 根据城市对与出发日期查询航班 右日期
      */
-    flightapi_flights_query: "/ba/flightapi/v1.0/flights/query",
+    flightapi_flights_query:            "/ba/flightapi/v1.0/flights/query",
+    flightapi_orderDetail_query:        "/ba/flightapi/v1.0/orderDetail/query",         //订单详情页  
+    orderapi_orders_create:             "/bo/orderapi/v1.0/orders/create",              //新增订单
 
     /**
-     * 订单详情页
+     * 需求相关接口
      */
-    flightapi_orderDetail_query: "/ba/flightapi/v1.0/orderDetail/query",
+    demandapi_demands:  "/bo/demandapi/v1.0/demands/create",            //新增需求
+    demand_list:        "/bo/demandapi/v1.0/demands/query",             //需求列表
+    demand_detail:      "/bo/demandapi/v1.0/demands/find",              //需求详情
+    demand_cancel:      "/bo/demandapi/v1.0/demands/cancel",            //需求取消
+    demand_remove:      "/bo/demandapi/v1.0/demands/remove",            //需求删除
+    demand_confirm:     "/bo/demandapi/v1.0/demands/plans/confirm",     //需求确认
 
     /**
-     * 新增订单
-     */
-    orderapi_orders_create: "/bo/orderapi/v1.0/orders/create",
+     * 审核相关接口
+     * 
+    */
+    pay_recordQuery:"/bo/orderapi/v1.0/orders/recordQuery",//记录查询
 
     /**
-     * 提交需求
-     */
-    //demandapi_demands: "/demandapi/v1.0/demands"
-    demandapi_demands: "/bo/demandapi/v1.0/demands/create",
+     * 公共服务接口
+    */
 
-    /**
-     * 需求列表
-     */
-    demand_list:"/bo/demandapi/v1.0/demands/query",
-
-    /**
-     * 需求详情
-     */
-    demand_detail: "/bo/demandapi/v1.0/demands/find",
-    /**
-     * 需求取消
-     */
-    demand_cancel: "/bo/demandapi/v1.0/demands/cancel",
-    /**
-     * 需求删除
-     */
-    demand_remove: "/bo/demandapi/v1.0/demands/remove",
-    /**
-     * 需求确认
-     */
-    demand_confirm: "/bo/demandapi/v1.0/demands/plans/confirm",
 };
 module.exports = APIGYW;
--- src/main/body/bank/BankUpload.js
+++ src/main/body/bank/BankUpload.js
@@ -4,6 +4,7 @@ import { HttpTool } from "../../../../lib/utils/index.js";
 import Panel from '../../../pay/Panel';
 import UploadCmp from './UploadCmp';
 import less from "./BankUpload.less";
+import APIGYW from "../../../api/APIGYW.js";
 
 
 class BankUpload extends Component {
@@ -36,7 +37,7 @@ class BankUpload extends Component {
 
    loadPayInfo2modify(param, cb) {
       HttpTool.request(HttpTool.typeEnum.POST,
-         "/bo/orderapi/v1.0/orders/recordQuery",
+         APIGYW.pay_recordQuery,
          (code, msg, json, option) => {
             cb(code, msg, json);
          }, (code, msg) => {
